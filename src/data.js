b9da8b113aa2c83df7a7c68899bf672f6ace6057
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-30 22:32:35 +0500

    Add Data

---
+++ src/data.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/prefer-default-export */
+import pro1 from './Assets/Projects/pro1.png';
+import pro2 from './Assets/Projects/pro2.png';
+import pro3 from './Assets/Projects/pro3.png';
+import pro4 from './Assets/Projects/pro4.png';
+
+const data = [
+  {
+    picture: pro1,
+    title: 'Everything You Need To Know About Online Coupons',
+  },
+  {
+    picture: pro2,
+    title: 'What Are Promotional Codes And How Do They Work?',
+  },
+  {
+    picture: pro3,
+    title: '10 pieces of pet furniture you should buy right now.',
+  },
+  {
+    picture: pro4,
+    title: 'Why NOW is The Time To Find Patio Furniture ',
+  },
+];
+export { data };
